6641a377067e12c4c1220179a315f3795b79598d
Brandon Nall <firewalkwithb@gmail.com>
2020-01-30 00:25:49 -0500

    Add Vacations Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import Vacations from './components/Vacations';
 import './App.css';
 import axios from 'axios';
 
 const API = 'https://acme-users-api-rev.herokuapp.com/api';
 
+//FETCHES RANDOM USER ON LOAD
 const fetchUser = async () => {
   const storage = window.localStorage;
   const userId = storage.getItem('userId');
@@ -21,13 +23,26 @@ const fetchUser = async () => {
   return user;
 };
 
-function App() {
-  const [user, setUser] = useState({});
+//FETCHES VACATION LIST BASED ON USER
+const fetchVacations = async () => {
+  const storage = window.localStorage;
+  const userId = storage.getItem('userId');
+  const vacations = (await axios.get(`${API}/users/${userId}/vacations`)).data;
+  return vacations;
+};
 
+function App() {
+  //USER AND VACATION LIST STATES
+  const [user, setUser] = useState({});
+  const [vacations, setVacations] = useState([]);
+
+  //ASSIGNS USER AND VACATION LIST TO STATE
   useEffect(() => {
     fetchUser().then(data => setUser(data));
+    fetchVacations().then(data => setVacations(data));
   }, [user]);
 
+  //ON CLICK RANDOMLY CHANGES USER
   const handleChangeUser = async e => {
     const storage = window.localStorage;
 
@@ -35,9 +50,43 @@ function App() {
     storage.setItem('userId', user.id);
   };
 
+  //ADDS VACATION TO LIST USING FORM DATA
+  const createVacation = async vacation => {
+    const response = await axios.post(
+      `${API}/users/${user.id}/vacations`,
+      vacation
+    );
+    setVacations([...vacations, response.data]);
+  };
+
+  const handleSubmitVacation = async e => {
+    e.preventDefault();
+
+    await createVacation({
+      startDate: e.target.elements[0].value,
+      endDate: e.target.elements[1].value,
+    });
+  };
+
+  //REMOVES VACATION FROM LIST
+  const handleRemoveVacation = async vacationToDestroy => {
+    await axios.delete(
+      `${API}/users/${user.id}/vacations/${vacationToDestroy.id}`
+    );
+    setVacations(
+      vacations.filter(vacation => vacation.id !== vacationToDestroy.id)
+    );
+  };
+
   return (
     <div id="app">
       <Header user={user} handleChangeUser={handleChangeUser} />
+      <Vacations
+        user={user}
+        vacations={vacations}
+        handleRemoveVacation={handleRemoveVacation}
+        handleSubmitVacation={handleSubmitVacation}
+      />
     </div>
   );
 }
---
+++ src/components/Vacations.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import moment from 'moment';
+
+function Vacations({
+  user,
+  vacations,
+  handleRemoveVacation,
+  handleSubmitVacation,
+}) {
+  const vacationList = vacations.map(function(vacation, i) {
+    const key = 'vacation' + i;
+    const x = moment(vacation.startDate);
+    const y = moment(vacation.endDate);
+    const duration = moment.duration(y.diff(x));
+    const days = duration.asDays();
+    return (
+      <div className="vacation" key={key}>
+        <h5>
+          Start Date :{' '}
+          {moment(vacation.startDate)
+            .add(1, 'day')
+            .format('dddd MM/DD/YYYY ')}
+        </h5>
+        <h5>
+          End Date :{' '}
+          {moment(vacation.endDate)
+            .add(1, 'day')
+            .format('dddd MM/DD/YYYY')}
+        </h5>
+        <h5>{days} Days</h5>
+        <button
+          className="removeVacation"
+          onClick={() => handleRemoveVacation(vacation)}
+        >
+          Remove Vacation
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div id="vacations">
+      <h3 className="header">VACATIONS</h3>
+      <form id="createVacation" onSubmit={handleSubmitVacation}>
+        <input id="startDate" type="date"></input>
+        <input id="endDate" type="date"></input>
+        <input id="submitVacation" type="submit" value="Create"></input>
+      </form>
+      <div id="vacationList">{vacationList}</div>
+    </div>
+  );
+}
+
+export default Vacations;
